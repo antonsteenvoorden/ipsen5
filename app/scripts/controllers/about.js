c139557fb27c21249c386a419c6ecbf19c78eb9d
Roy Touw <roytouw@hotmail.com>
2016-03-24 09:43:31 +0100

    yeoman app gen

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wfpcsFrontApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the wfpcsFrontApp
+ */
+angular.module('wfpcsFrontApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
