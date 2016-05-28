73f91ff48680d8c79d01f7770497e0f031bac10b
Anton <antons@live.nl>
2016-05-28 17:19:25 +0200

    Changed some directive names. Changed the way you add new process steps. Changed the way the processtep gets listed

---
+++ app/scripts/directives/processStep.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Anton on 2-4-2016.
+ */
+
+'use strict';
+angular.module('wfpcsFrontApp').directive("processstep", function () {
+  return {
+
+    templateUrl: 'views/analyse/process-step.html'
+  };
+});
