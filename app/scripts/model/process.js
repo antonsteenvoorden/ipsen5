7e776bf4ff00f5846fc4c48d864f3b00012d5582
Anton <antons@live.nl>
2016-06-08 23:09:42 +0200

    rename vars

--- app/scripts/model/process.js
+++ app/scripts/model/process.js
@@ -8,8 +8,8 @@ var Process = function(id, name, date, batchSize, hoursDay, piecesDay, stepAmoun
   this.name = name;
   this.date = date;
   this.batchSize = batchSize;
-  this.hoursDay = hoursDay;
-  this.piecesDay = piecesDay;
+  this.hoursPerDay = hoursDay;
+  this.piecesPerDay = piecesDay;
   this.stepAmount = stepAmount | this.getStepAmount();
   this.position = position | NaN; //@TODO: position should get fetched from database.
 };
@@ -32,5 +32,5 @@ Process.prototype.getStepAmount = function() {
  */
 Process.prototype.clone = function() {
   return new Process(this.id, this.name, this.date, this.batchSize,
-    this.hoursDay, this.piecesDay, this.stepAmount, this.position);
+    this.hoursPerDay, this.piecesPerDay, this.stepAmount, this.position);
 };
