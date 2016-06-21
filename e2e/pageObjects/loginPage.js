173af673b7c5efeb4ec9d274331e545807d6c915
Anton <antons@live.nl>
2016-06-21 12:24:31 +0200

    Login test written

--- e2e/pageObjects/loginPage.js
+++ e2e/pageObjects/loginPage.js
@@ -4,8 +4,43 @@
 var LoginPage = function() {
   var self = this;
 
-  self.getTrue = function(){
-    return true;
+  var credentials = {
+    admin: {
+      username:'test',
+      password:'1234'
+    },
+    guest: {
+      username:'anton',
+      password:'sexy'
+    }
   };
+
+  var usernameField = element(by.id('login.username'));
+  var passwordField = element(by.id('login.password'));
+  var submitButton = element(by.id('login.submit'));
+
+  self.get = function() {
+    browser.get(browser.baseUrl + 'login');
+  };
+
+  self.enterCredentials = function(user){
+    var keys = credentials[user || 'guest'];
+    usernameField.sendKeys(keys.username);
+    passwordField.sendKeys(keys.password);
+    submitButton.click();
+  };
+
+  self.getUrl = function () {
+    return browser.getCurrentUrl();
+  };
+
+  self.urlContains = function (stringToCheck) {
+    return new Promise(function (fulfill, reject) {
+      self.getUrl().then(function (currentUrl) {
+        (currentUrl.includes(stringToCheck) ? fulfill(true) : reject(false));
+      });
+    });
+  };
+
 };
 module.exports = LoginPage;
--- e2e/specs/login/login-spec.js
+++ e2e/specs/login/login-spec.js
@@ -10,8 +10,21 @@ var LoginPage = require('../../pageObjects/LoginPage.js');
 describe('Try to login',function(){
   var loginPage = new LoginPage();
 
-  it('should open the login page', function(done){
-    expect(loginPage.getTrue()).to.equal(true);
-    done();
+  it('should open the login page', function(done) {
+    loginPage.get();
+    loginPage.urlContains('login').then(function(result){
+      expect(result).to.equal(true);
+      done();
+    });
+
   });
+
+  it('should enter credentials and log in', function(done) {
+    loginPage.enterCredentials('admin');
+    loginPage.urlContains('dashboard').then(function(result){
+      expect(result).to.equal(true);
+      done();
+    });
+  });
+
 });
