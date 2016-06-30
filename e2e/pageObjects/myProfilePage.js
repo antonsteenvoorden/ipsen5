80377dda95b7c576f26127e598f5af218420775e
Anton <antons@live.nl>
2016-06-30 10:26:34 +0200

    prep for myprof

---
+++ e2e/pageObjects/myProfilePage.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Anton on 02/06/2016.
+ */
+var MyProfilePage = function() {
+  var self = this;
+
+  self.getTrue = function(){
+    return true;
+  };
+};
+module.exports = MyProfilePage;
